822ed58314cdd7237c615173910b51ae7c68055e
Will Franzen <wtfranzen@gmail.com>
2014-02-19 00:40:17 -0600

    More work on this code...
    I desperately need sleep

--- powernode.js
+++ powernode.js
@@ -1,7 +1,8 @@
+this.https = require('https');
+this.querystring = require('querystring');
+
 module.exports = function(host) {
-	
-	this.http = require('http');
-	
+		
 	this.host = host;
 	this.userAgent = 'powernode/0.0.0 (https://github.com/wtfaremyinitials/powernode)';
 	
@@ -10,10 +11,15 @@ module.exports = function(host) {
 
 		var username = '';
 		var password = '';
-		var session  = '';	
+		var cookie   = '';
+		
+		var authData = {
+			'pstoken': '',
+			'contextData': ''
+		};	
 		
 		var classes = [];
-
+		
 		function isLoggedIn() {
 			return session != '';
 		}
@@ -21,54 +27,100 @@ module.exports = function(host) {
 		function auth(username, password) {
 			this.username = username;
 			this.password = password;
-			this.getCSRFToken();
+			this.getAuthData();
 		}
 		
-		function getCSRFToken() {
-			http.request({
-				hostname: host,
-				port: 80,
-				path: '/public/home.html',
-				method: 'GET',
-				headers: {
-					'UserAgent': userAgent
-				}
-			}, function(res) {
-				if(res.statusCode != 200) {
-					// Uh, oh
-				} else {
-					var chunks = [];
-					res.on('data', function(data){
-						chunks.push(data);
-					});
-					res.on('end', function(){
-						var body = new Buffer(chunks.length);
-						var copied = 0;
-						chunks.forEach(function(chunk) {
-							chunk.copy(body, copied, 0);
-							copied += chunk.length;
-						});
-						// Now, to find the token. I feel like I'm falling deeper into the rabbit hole
-					});
-				}
-			}).on('error', function(e) {
-				// Uh, oh
+		function getAuthData() {
+			get('/public/home.html', cookie, function(body, newCookie) {
+				cookie = newCookie;
+				
+				var pstokenRegex = /.*<input type="hidden" name="pstoken" value="(.*)" \/>.*/ // I hate regex
+				var contextDataRegex = /.*<input type="hidden" name="contextData" value="(.*)" \/>.*/
+				
+				authData['pstoken']     = body.match(pstokenRegex)[1];
+				authData['contextData'] = body.match(contextDataRegex)[1];
 			});
 		}
 		
 		function sendLoginData() {
-			http.request({
-				
-			}, loginComplete);
-		}
-		
-		function loginComplete() {
+			data = {
+				'pstoken': authData['pstoken'],
+				'contextData': authData['contextData'],
+				'serviceName': 'PS Parent Portal', // Is this some kind of magic value? TODO
+				'pcasServerUrl': '/',
+				'credentialType': 'User Id and Password Credential',
+				'account': username,
+				'pw': psHashPassword(password)
+			}
 			
+			post('/guardian/home.html', cookie, querystring.stringify(data), function(body, newCookie) {
+				
+			});
 		}
 	
 	}
 	
+	function get(path, cookie, callback) {
+		var options = {
+			host: host,
+			path: path,
+			method: 'GET',
+			headers: {
+				'UserAgent': userAgent,
+				'Cookie': (typeof cookie != 'undefined')? (cookie) : ('')
+			}
+		}
+	
+		https.request(options, function(res){
+			var body = '';
+			res.on('data', function(data){
+				body += data;
+			});
+			res.on('end', function() {
+				var cookie = res.headers['Set-Cookie'];
+				if(cookie) {
+					cookie = (cookie + "").split(";").shift()
+				}
+				
+				callback(body, cookie);
+			});
+		}).end();
+	}
+	
+	function post(path, cookie, data, callback) {
+		var options = {
+			host: host,
+			path: path,
+			method: 'POST',
+			headers: {
+				'UserAgent': userAgent,
+				'Cookie': (typeof cookie != 'undefined')? (cookie) : ('')
+			}
+		}
+		
+		https.request(options, function(res){
+			var body = '';
+			res.on('data', function(data){
+				body += data;
+			});
+			res.on('end', function() {
+				var cookie = res.headers['Set-Cookie'];
+				if(cookie) {
+					cookie = (cookie + "").split(";").shift()
+				}
+				
+				callback(body, cookie);
+			});
+		}).write(data).end();
+	}
+	
 }  
+
+// TODO Password hash/encrypt thing
+function psHashPassword(pass) {
+	// hmac.new(authdata['contextData'].encode('ascii'), base64.b64encode(hashlib.md5(pw.encode('ascii')).digest()).replace(b"=", b""), hashlib.md5).hexdigest()
+	return pass;
+}
 
 function Class() {
 	
