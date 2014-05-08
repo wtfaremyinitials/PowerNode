49ff063727f52b6eeabb73209943c05ae9bb3002
Will Franzen <wtfranzen@gmail.com>
2014-05-08 17:39:50 -0500

    Added a chmod +x for commit hook

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
         },
         shell: {
             hooks: {
-                command: 'cp git-hooks/pre-commit .git/hooks/'
+                command: 'cp git-hooks/pre-commit .git/hooks/ && chmod +x .git/hooks/pre-commit'
             }
         }
     });
