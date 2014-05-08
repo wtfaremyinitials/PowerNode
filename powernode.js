f551141a729742e7fa1d4f420998b62fd56a86a5
Will Franzen <wtfranzen@gmail.com>
2014-05-08 18:22:30 -0500

    Replaced http module with Q-IO and started rewriting

--- powernode.js
+++ powernode.js
@@ -1,91 +1,26 @@
 /*
 	Packages
  */
-var Q     = require('q');
-var https = require('https');
+var Q = require('q');
+var HTTP = require('q-io/http');
 
 /*
-	Variables
+	Constants
 */
 
 // User Agent sent to PowerSchool server
 var userAgent = 'powernode/0.0.1 (https://github.com/wtfaremyinitials/powernode)';
 
 /*
-	Util Methods
-*/
-
-// Performs an HTTPS POST request
-var post = function(hostname, path, cookie, data) {
-	return Q.fcall(function(hostname, path, cookie, data) {
-		var options = {
-			host: hostname,
-			path: path,
-			method: 'POST',
-			headers: {
-				'UserAgent': userAgent,
-				'Cookie': (cookie || '')
-			}
-		};
-
-		https.request(options, function(res){
-			var body = '';
-
-			res.on('data', function(data){
-				body += data;
-			});
-
-			res.on('end', function() {
-				return {
-					"body": body,
-					"cookie": (res.headers['Set-Cookie'] || '')
-				};
-			});
-
-		}).end(data);
-	});
-};
-
-var get = function(hostname, path, cookie) {
-	return Q.fcall(function(hostname, path, cookie) {
-		var options = {
-			host: hostname,
-			path: path,
-			method: 'GET',
-			headers: {
-				'UserAgent': userAgent,
-				'Cookie': (cookie || '')
-			}
-		};
-
-		https.request(options, function(res){
-			var body = '';
-
-			res.on('data', function(data){
-				body += data;
-			});
-
-			res.on('end', function() {
-				return {
-					"body": body,
-					"cookie": res.headers['Set-Cookie']
-				};
-			});
-
-		});
-	});
-};
-
-/*
 	Models
 */
-var Student = function() {
+var Student = function(hostname, username, password) {
 	this.hostname = "";
 	this.cookie   = "";
 
 	this.authData = {
-		"pstoken": "",
-		"contextData": ""
+		pstoken: "",
+		contextData: ""
 	};
 
 };
@@ -100,29 +35,54 @@ Student.prototype.getGrades = function() {
 /*
 	Functions
 */
+var authenticate = function() {
+	requestIndex().
+		then(parseIndex).
+		then(hashData).
+		then(requestLogin).
+		then(parseHome);
+};
 
-var getAuthenticationData = function(student) {
-	get(student.hostname, '/public/', student.cookie).then();
+var requestIndex = function(student) {
+	HTTP.request({
+		scheme: 'https:',
+		host: student.hostname,
+		path: '/public/home.html',
+		port: 443
+	}).then(parseIndex);
 };
 
 var parseIndex = function(response) {
 	var body = response.body;
 
 	var pstokenRegex = /<input type="hidden" name="pstoken" value="([a-z0-9]*)" \/>/g;
-	var contextDataRegex = /<input type="hidden" name="contextData" value="([A-Z0-9]*)" \/>/g;
+	var contextDataRegex = /<input type="hidden" name="contextData" value="([A-Z0-9]*)" \/>/g; // AKA pskey
+
+	student.pstoken = body.match(pstokenRegex);
+	student.contextData = body.match(contextDataRegex);
 };
 
-var prepareAuthData = function() {
+// Hash important powerschool data
+var hashData = function() {
 
 };
 
+// Perform the login request
+var requestLogin = function() {
+
+};
+
+// Parses grades on homepage
+var parseHome = function() {
+
+};
 
 var loginStudent = function(hostname, username, password) {
 	return Q.fcall(function() {
-
-
-
-
+		var student = new Student(hostname, username, password);
+		authenticate(student).then(function() {
+			resolve(student);
+		});
 	});
 };
 
