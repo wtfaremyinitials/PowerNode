382cf092811761364e0d4e643e48e06afe162f70
Will Franzen <wtfranzen@gmail.com>
2014-05-24 19:07:38 -0500

    Start re-write

--- powernode.js
+++ powernode.js
@@ -1,4 +1,48 @@
 /*
 	Packages
  */
-var Q = require('Q');
+var Q      = require('Q');
+var xml2js = require('xml2js');
+var https  = require('https');
+
+/*
+	Generic Methods
+*/
+var request = Q.denodify(https.request);
+var parseXML = Q.denodify(xml2js.parseString);
+
+/*
+	Constants
+*/
+var userAgent = 'powernode/0.0.0 (https://github.com/wtfaremyinitials/powernode)';
+
+module.exports.getStudentData = function(hostname, username, password) {
+	var contextData = '';
+	var pstoken = '';
+	var cookie = '';
+	return getIndex(hostname).then(parseIndex).then(prepareLogin).then(requestLogin).then(downloadXML).then(parseXML).then(convert);
+};
+
+var getIndex = function(hostname) {
+
+};
+
+var parseIndex = function(response) {
+
+};
+
+var prepareLogin = function() {
+
+};
+
+var requestLogin = function() {
+
+};
+
+var downloadXML = function() {
+
+};
+
+var convert = function(studentData) {
+
+};
