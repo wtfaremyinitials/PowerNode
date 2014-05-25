e8a0e0acda71d92249d35062a5dd799b22d48270
Will Franzen <wtfranzen@gmail.com>
2014-05-24 23:11:40 -0500

    Change cookie handling

--- powernode.js
+++ powernode.js
@@ -4,7 +4,7 @@
 var Q      = require('Q');
 var xml2js = require('xml2js');
 var https  = require('https');
-var cookie = require('cookie');
+var toughcookie = require('tough-cookie');
 var querystring = require('querystring');
 var pscrypto = require('./lib/pscrypto');
 
@@ -39,22 +39,6 @@ var hashPassword = function(contextData, password) {
 
 var generateDBPW = function(contextData, password) {
     return pscrypto.hex_hmac_md5(contextData, password.toLowerCase());
-};
-
-var mergeCookies = function(currentCookie, newCookies) {
-	currentCookie = cookie.parse(currentCookie);
-
-	for(var i=0; i<newCookies.length; i++) {
-		newCookies[i] = cookie.parse(newCookies[i]);
-	}
-
-	for(var j=0; i<newCookies.length; i++) {
-		for (var attrname in newCookies[j]) {
-			currentCookie[attrname] = newCookies[attrname];
-		}
-	}
-
-	return cookie.serialize(currentCookie);
 };
 
 var parseXML = Q.denodeify(xml2js.parseString);
@@ -100,7 +84,7 @@ var getIndex = function(state) {
 		'hostname': state.hostname
 	}).then(function(response) {
 		state.index = response.body;
-		state.cookie = mergeCookies(state.cookie, response.cookie);
+		state.cookie = mergeCookies(state.cookie, response.cookie); // TODO: Replace with tough-cookie cookiejar
 		return state;
 	});
 };
@@ -146,7 +130,7 @@ var requestLogin = function(state) {
 		'hostname': state.hostname,
 		'Cookie': state.cookie
 	}, state.loginData).then(function(response) {
-		state.cookie = mergeCookies(state.cookie, response.cookie);
+		state.cookie = mergeCookies(state.cookie, response.cookie); // TODO: Replace with tough-cookie cookiejar
 		return state;
 	});
 };
