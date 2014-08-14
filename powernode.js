7017dd96dab83c7e8fa6d65647777ccd4a540f43
Will Franzen <wtfranzen@gmail.com>
2014-08-14 15:24:21 -0700

    It works =D

--- powernode.js
+++ powernode.js
@@ -9,11 +9,13 @@ var getStudentData = function(hostname, username, password) {
     subprocess.execFile(binPath, [scriptPath, hostname, username, password], function(err, stdout, stderr) {
         if(stdout == 'BADPASS')
             deferred.reject('BADPASS');
+        if((stdout+'').indexOf('CasperError') != -1)
+            deferred.reject('INTERNAL');
         if(stdout == '')
             deferred.reject('UNKNOWN');
         deferred.resolve(stdout);
     });
-    return deferred;
+    return deferred.promise;
 };
 
 module.exports.getStudentData = getStudentData;
