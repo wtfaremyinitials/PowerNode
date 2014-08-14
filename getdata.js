4d5e0cb8a5bb8a3f223ea27ba452a2902e344ae8
Will Franzen <wtfranzen@gmail.com>
2014-08-14 11:34:21 -0700

    Write getdata.js

---
+++ getdata.js
@@ -0,0 +1,28 @@
+var casper = require('casper').create();
+
+var hostname = casper.cli.args[0];
+var username = casper.cli.args[1];
+var password = casper.cli.args[2];
+
+var baseURL = 'https://' + hostname;
+
+var fillForm = function() {
+    this.fill('form[name="LoginForm"]', {
+        account: username,
+        pw: password
+    }, true);
+};
+
+var checkSuccess = function() {
+    this.evaluateOrDie(function() {
+        return /Grades and Attendance/.test(document.body.innerText)
+    });
+};
+
+var dumpPageContent = function() {
+    this.echo(this.getPageContent());
+};
+
+casper.start(baseURL + '/public/', fillForm);
+casper.then(checkSuccess);
+casper.thenOpen(baseURL + '/guardian/studentdata.xml', dumpPageContent);
